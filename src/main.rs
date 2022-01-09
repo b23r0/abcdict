1c183da8df02b3d6a1cfd4036bc9b8b3c713b839
b23r0 <b23r0@foxmail.com>
2022-01-09 17:57:33 +0800

    update framework.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+mod parser;
+
+fn usage () {
+	println!("abcdict - Password Dictionary Generator");
+	println!("https://github.com/b23r0/abcdict");
+	println!("Usage: abcdict \"pass[n1-100]word[sabc|456]\"");
+}
+
 fn main() {
+    
+    if std::env::args().count() != 2{
+        usage()
+    }
+
+    let d = std::env::args().nth(1).unwrap().as_str().to_string();
+
+    parser::exec(d).unwrap();
+
     println!("Hello, world!");
 }
